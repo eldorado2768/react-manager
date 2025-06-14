2b44a5a3cd3301fd205d9ecea89f998748dc5f43
David Brotman <david@123websitebiz.com>
2025-06-13 17:08:59 -0700

    update env variables

--- src/hooks/useSupabase.js
+++ src/hooks/useSupabase.js
@@ -2,8 +2,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 
-console.log("SUPABASE_URL", process.env.REACT_APP_SUPABASE_URL);
-console.log("SUPABASE_ANON_KEY", process.env.REACT_APP_SUPABASE_URL);
+console.log("REACT_APP_SUPABASE_URL", process.env.REACT_APP_SUPABASE_URL);
+console.log("REACT_APP_SUPABASE_ANON_KEY", process.env.REACT_APP_SUPABASE_URL);
 
 const useSupabase = () => {
   const [supabaseClient, setSupabaseClient] = useState(null);
